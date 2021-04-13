ccd199199b4963f9a52e079f5713fd015140680b
Ali Kabalan <ali.kabalan.1994@gmail.com>
2021-04-13 20:45:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solutions in this file
+//No.1
+
+let names = ["Ada", "Brendan", "Ali"];
+
+function writeCards(names , birthday) {
+  for (let i = 0; i < names.length; i++) {
+console.log(`Thank you, ${names[i]}, for the wonderful ${birthday} gift!`);
+}
+
+return names;
+}
+
+writeCards(name, 'birthday');
+
+
+
+
+//No.2
+let number = 15;
+
+function countDown(number) {
+while (number >= 0) {
+console.log(number--);
+}
+
+return countDown;
+}
+
+countDown(number);
